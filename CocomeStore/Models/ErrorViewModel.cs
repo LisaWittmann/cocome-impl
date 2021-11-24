e1550485e90e53ad39d109b9da70d9deb3baed02
ThirasByron <50230996+ThirasByron@users.noreply.github.com>
2021-11-24 10:36:53 +0100

    Das alte Projekt gelöscht und durch ein neues mit MVC und Angular ersetzt.

---
+++ CocomeStore/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace CocomeStore.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
